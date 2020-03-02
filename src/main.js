a2f257855739119b4a534a95313910a851d3dcfa
YanQiMing <1024744235@qq.com>
2020-03-02 16:51:46 +0800

    project init

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import fastClick from 'fastclick'
+
+Vue.config.productionTip = false;
+fastClick.attach(document.body);
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
