7ed90aeb1a7cf9a750084e4f19d01055fe1d0e85
YanQiMing <1024744235@qq.com>
2020-03-04 23:44:50 +0800

    mock数据的引入

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -16,15 +16,27 @@
     import HomeIcons from './components/Icons';
     import HomeRecommend from './components/Recommend';
     import HomeWeekend from './components/Weekend';
-
+    import axios from 'axios';
     export default {
         name: 'Home',
         components: {
-          HomeSwiper,
-          HomeHeader,
-          HomeIcons,
-          HomeRecommend,
-          HomeWeekend
+            HomeSwiper,
+            HomeHeader,
+            HomeIcons,
+            HomeRecommend,
+            HomeWeekend
+        },
+        methods: {
+            getHomeInfo() {
+                axios.get('/static/mock/index.json')
+                        .then(this.getHomeInfoSucc);
+            },
+            getHomeInfoSucc(res) {
+                console.log(res);
+            }
+        },
+        mounted() {
+            this.getHomeInfo();
         }
     };
 </script>
